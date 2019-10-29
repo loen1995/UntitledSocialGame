de2b4fff1e910f91fa9651f8d90931901993c562
Adrián <loen1995@gmail.com>
2019-10-29 18:58:33 +0100

    Creation BootActivity

---
+++ app/src/main/java/com/gimbernat/UntitledSocialGame/scenes/boot/BootActivity.java
@@ -0,0 +1,17 @@
+package com.gimbernat.UntitledSocialGame.scenes.boot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gimbernat.UntitledSocialGame.R;
+
+public class BootActivity extends AppCompatActivity {
+    //test
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
