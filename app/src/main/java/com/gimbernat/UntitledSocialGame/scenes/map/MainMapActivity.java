fe5c9a24723144df750204e59e1ae969b3fc6232
jordiEUG <jlopezay@campus.eug.es>
2019-11-05 19:06:35 +0100

    Commit inicial con private y public bas ehecho para empezar a desarrollar

---
+++ app/src/main/java/com/gimbernat/UntitledSocialGame/scenes/map/MainMapActivity.java
@@ -0,0 +1,16 @@
+package com.gimbernat.UntitledSocialGame.scenes.map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gimbernat.UntitledSocialGame.R;
+
+public class MainMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_map);
+    }
+}
