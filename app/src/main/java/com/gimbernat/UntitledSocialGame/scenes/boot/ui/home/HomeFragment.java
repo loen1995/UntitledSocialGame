775d3167f3387ae11f4739ea9b49c63101d67a79
jordiEUG <jlopezay@campus.eug.es>
2019-12-28 13:35:26 +0100

    Intento de unir mapa y menú

--- app/src/main/java/com/gimbernat/UntitledSocialGame/scenes/boot/ui/home/HomeFragment.java
+++ app/src/main/java/com/gimbernat/UntitledSocialGame/scenes/boot/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.gimbernat.UntitledSocialGame.scenes.boot.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,16 +14,28 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.gimbernat.UntitledSocialGame.R;
+import com.gimbernat.UntitledSocialGame.scenes.boot.BootActivity;
+import com.gimbernat.UntitledSocialGame.scenes.boot.MainActivity;
+import com.gimbernat.UntitledSocialGame.scenes.map.GoogleMapsActivity;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private GoogleMapsActivity map;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        //Esto es lo que carga el layout del menu, pero no se inicializa correctamente el activity del GoogleMapsActivity
         View root = inflater.inflate(R.layout.activity_google_maps, container, false);
+
+        //Esto es lo que te abre el mapam y te pisa el menu
+//        Intent intent = new Intent(getActivity(), GoogleMapsActivity.class);
+//        startActivity(intent);
+
+
+
         /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
