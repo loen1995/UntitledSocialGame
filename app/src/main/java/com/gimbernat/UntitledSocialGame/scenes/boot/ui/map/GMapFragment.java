bf1765b443f1a8c6735d1ebe937eda4a586aa3de
Adrián <56130139+loen1995@users.noreply.github.com>
2019-12-31 11:41:51 +0100

    V3.2 MenuMap ----------------------------------    + Se han corregido diversos bugs    + Se ha añadido la funcionalidad de centrar la cámara en la ubicación    + Se ha añadido la funcionalidad de interactuar con el icono de la ubicación actual    + Se ha conseguido guardar la localización en variables locales para trabajar más adelante con eventos    - Se ha eliminado código obsoleto

--- app/src/main/java/com/gimbernat/UntitledSocialGame/scenes/boot/ui/map/GMapFragment.java
+++ app/src/main/java/com/gimbernat/UntitledSocialGame/scenes/boot/ui/map/GMapFragment.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +18,8 @@ import androidx.fragment.app.Fragment;
 
 import com.gimbernat.UntitledSocialGame.R;
 import com.gimbernat.UntitledSocialGame.scenes.boot.MainActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,23 +28,61 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-//Imports de localización
 
-public class GMapFragment extends SupportMapFragment implements OnMapReadyCallback {
+public class GMapFragment extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
 
-    //Variables
     private GoogleMap mMap;
     private Marker marcador;
-    double lat = 10.0;
-    double lng = 20.0;
+    private FusedLocationProviderClient fusedLocationClient;
+    double lat;
+    double lng;
 
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        getMapAsync(this);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        return rootView;
+    }
 
-    //Función que se activa cuando el mapa está preparado
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+    }
+
+    //Botón centrar
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(getActivity() , "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    //Indicador Ubicación
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        //Toast.makeText(getActivity(), "Current location:\n" + location, Toast.LENGTH_LONG).show();
+        getLastLocation();
+    }
+
+    //Función para coger la última ubicación
+    public void getLastLocation(){
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        //A veces tira null, pongo esto para control de errores
+                        if (location != null) {
+                            Toast.makeText(getActivity(), "Current location:\n" + location, Toast.LENGTH_LONG).show();
+                            lat = location.getLatitude();
+                            lng = location.getLongitude();
+                            //agregarMarcador(lat, lng);
+                        }
+                    }
+                });
     }
 
     private void agregarMarcador(double lat, double lng) {
@@ -57,28 +98,5 @@ public class GMapFragment extends SupportMapFragment implements OnMapReadyCallba
         //Animación para enfocar donde está la cámara
         mMap.animateCamera(miUbicacion);
     }
-
-    /*
-    public void actualizarUbicacion(Location location) {
-        //Si no hay ubicación, la pido
-        if (location != null) {
-            lat = location.getLatitude();
-            lng = location.getLongitude();
-            agregarMarcador(lat, lng);
-        }
-    }
-*/
-
-    //Fragment SIN ACTIVITY
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        View rootView = super.onCreateView(inflater, container, savedInstanceState);
-
-        getMapAsync(this);
-
-        return rootView;
-    }
-
 
 }
